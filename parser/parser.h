71fb18446cdbfccfed197bfe070d3b5ab6916a92
Informhunter <informhunter@gmail.com>
2015-01-04 00:36:32 +0300

    Declared parser function

--- parser/parser.h
+++ parser/parser.h
@@ -1,6 +1,15 @@
 #ifndef _PARSER_H_INCLUDED
 #define _PARSER_H_INCLUDED
 
+typedef enum
+{
+	MinPriority,
+	PriorityPlusMinus,
+	PriorityMulDiv,
+	MaxPriority,
+	BadPriority,
+} OperatorPriority;
 
+Tree* buildTreeForExpression(char* expression);
 
 #endif

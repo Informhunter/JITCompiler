2e9ba2d9d9f633f1df1b72ae5a4044f8bbfa6af8
Informhunter <informhunter@gmail.com>
2015-02-12 22:40:42 +0300

    Added CompiledFunc struct. Redelcared compiling func.

--- compiler/compiler.h
+++ compiler/compiler.h
@@ -2,10 +2,17 @@
 #define _COMPILER_H_INCLUDED
 
 #include "tree/tree.h"
+#include "bytearray/bytearray.h"
 
 typedef double (*Func)(double);
 
-Func compileTree(Tree* tree);
+typedef struct
+{
+	ByteArray* data;
+	Func function;
+} CompiledFunc;
+
+CompiledFunc compileTree(Tree* tree);
 
 
 #endif

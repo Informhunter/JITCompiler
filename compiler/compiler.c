680884371ded5643423f82c144334ee6d48c97a4
Informhunter <informhunter@gmail.com>
2015-03-05 11:30:29 +0300

    Started compiler code

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -1,1 +1,51 @@
 #include "compiler.h"
+#include "bytearray/bytearray.h"
+#include <Windows.h>
+
+static void generateCodeR(TreeNode* root, ByteArray* resultCode)
+{
+	ByteArray* rc = resultCode;
+	generateCodeR(root->left, rc);
+	generateCodeR(root->right, rc);
+	switch(root->type)
+	{
+		case OperatorPlus:
+
+		case OperatorMinus:
+
+		case OperatorMul:
+
+		case OperatorDiv:
+
+		case OperandVar:
+
+		case OperandNegVar:
+
+		case OperandConst:
+			
+	}
+}
+
+static void generateCode(Tree* tree, ByteArray* resultCode)
+{
+	ByteArray* rc = resultCode;
+	//Some stack allocation stuff + argument parsing
+	
+	//Calculating
+	generateCodeR(tree->root, rc);
+	//Return value stuff
+}
+
+Func compileTree(Tree* tree);
+{
+	DWORD oldP;
+	char* resultCode;
+	ByteArray* code;
+	code = byteArrayCreate(2);
+	generateCode(tree, code);
+	resultCode = malloc(code->dataSize);
+	memcpy(resultCode, code->data, code->dataSize);
+	VirtualProtect(resultCode, code->dataSize, PAGE_EXECUTE_READ_WRITE, &oldP);
+	ByteArrayFree(code);
+	return (Func) resultCode;
+}
